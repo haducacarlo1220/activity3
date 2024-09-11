35308a47101331a88fdbdac1b48f8cefe093f298
haducacarlo1220 <ralphhaduca@gmail.com>
2024-09-11 23:03:47 +0800

    initial commit with Number.js

---
+++ Number.js
@@ -0,0 +1,21 @@
+let firstFavNum = 3;
+let secondFavNum = 20;
+
+//addition
+let sum = firstFavNum + secondFavNum;
+console.log(sum);
+//subtraction
+let difference = firstFavNum - secondFavNum;
+console.log(difference);
+// multiplication
+let product = firstFavNum * secondFavNum;
+console.log(product);
+//division
+let qoutient = firstFavNum / secondFavNum;
+console.log(qoutient);
+//modulus
+let remainder = firstFavNum % secondFavNum;
+console.log(remainder);
+//exponentiation
+let power = firstFavNum ** secondFavNum;
+console.log(power);
